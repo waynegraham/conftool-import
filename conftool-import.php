cba4453754259063a4afbd380847aeaf0a5be3fd
Wayne Graham <wgraham@clir.org>
2017-04-26 11:21:57 -0400

    Initial scaffold for plugin

---
+++ conftool-import.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     ConfTool Wordpress Import
+ * Plugin URI:      https://github.com/waynegraham/conftool-wordpress-import
+ * Description:     Import Conftool for ShowTheme custom posts
+ * Author:          YOUR NAME HERE
+ * Author URI:      https://www.clir.org/
+ * Text Domain:     conftool-import
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Conftool_Import
+ */
